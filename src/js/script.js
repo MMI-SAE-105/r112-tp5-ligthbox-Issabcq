a75f7415dc7c2ebce9b0bf5e2356457544ae91ef
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-12-04 11:37:29 +0100

    Complétion du JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 // *** LIGHTBOX ***
-    
+document.addEventListener("DOMContentLoaded", () => {
+    const thumbnails = document.querySelectorAll('ul li img');
+    const lightbox = document.querySelector('#lightbox');
+    const lightboxImage = document.querySelector('#lightbox img');
+
+    thumbnails.forEach((thumbnail) => {
+        thumbnail.addEventListener('click', () => {
+            const fullImageUrl = thumbnail.getAttribute('data-full-img');
+            lightboxImage.setAttribute('src', fullImageUrl);
+            lightbox.showModal();
+        });
+    });
+
+    lightbox.addEventListener('click', () => {
+        lightbox.close();
+    });
+});
